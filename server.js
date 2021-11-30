c5f88865f6b643f3fb0b5f844589c125e31374a1
Rabindra Nath <rabbinath@gmail.com>
2021-11-30 07:40:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -10,6 +14,69 @@ app.get('/', (req, res) => {
 });
 
 
+let uri='mongodb+srv://rabbinath:'+ process.env.PW +'@cluster0.0zmv9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(uri,{useNewUrlParser: true,useUnifiedTopology: true});
+
+
+let exerciseSchema=new mongoose.Schema(
+  {
+  username:{type:String},
+  description:String,
+  dusration:Number,
+  date:Date
+})
+
+let userSchema=new mongoose.Schema(
+{
+  username: String
+})
+
+let logSchema=new mongoose.Schema(
+{
+  username: String,
+  count: Number,
+    log: [{
+    description: String,
+    duration: Number,
+    date: Date
+  }]
+})
+
+let Exercise=mongoose.model('Exercise',exerciseSchema)
+let User=mongoose.model('User',userSchema)
+let Log=mongoose.model('Log',logSchema)
+
+app.post('/api/users',bodyParser.urlencoded({ extended: false }),function(req,res){
+  let inputUsername=req.body['username'];
+  let idUsername=0
+  User.findOne({})
+      .sort({username:'desc'})
+      .exec((errUser,resultUser) => 
+      {
+        if(!errUser && typeof resultUser!=='undefined')
+        {
+          inputUsername=resultUser.username
+          idUsername=resultUser._id
+        }
+          if(!errUser)
+          {
+            User.findOneAndUpdate(
+              {username:inputUsername},
+              {new:true,upsert:true},
+              (err,saveUser)=>{
+                if(!err){
+                 res.json(saveUser)
+                }
+              }
+            )
+          }
+      }
+      
+        )
+    
+
+
+})
 
 
 
