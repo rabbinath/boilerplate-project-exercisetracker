3cf67c7707a968f0136cc16c883468db72d54b01
Rabindra Nath <rabbinath@gmail.com>
2021-12-01 20:00:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ app.get('/api/users',function(req,res){
 
 app.post('/api/users/:_id/exercises',bodyParser.urlencoded({ extended: false }),function(req,res){
     let inputUsername=''
-  let inputId =new ObjectId(req.body['_id'])
+  //let inputId =new ObjectId(req.body['_id'])
+  let inputId =new req.body['_id']
   let inputDescription=req.body['description'];
   let inputDuration=req.body['duration'];
   let inputDate=req.body['date'];
@@ -82,15 +83,13 @@ app.post('/api/users/:_id/exercises',bodyParser.urlencoded({ extended: false }),
   }   
   User.findOne({inputId}, function(errById,userFound){
    if(errById) return res.json({error: "Could not find user" });
-   
-  
-  
- const exerInpput={
+
+
+const exerInpput={
   description:inputDescription,
   duration:inputDuration,
   date:inputDate
-
- }
+}
 
  userFound.push(exerInpput)
  if(userFound.count){
