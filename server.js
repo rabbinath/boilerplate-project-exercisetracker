9e44ec386ee3f6d0461904eaca02c24a2a7a0c3a
Rabindra Nath <rabbinath@gmail.com>
2021-12-13 18:15:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ let exerciseSchema=new mongoose.Schema(
   username:{type:String, required:true, unique:false},
   description:{type:String,required:true},
   duration:{type:Number,required:true},
-  date:{type:String, required:false}
+  date:{type:String, required:false},
+  versionKey: false
 })
 
 let userSchema=new mongoose.Schema(
